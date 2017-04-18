11089a3b559db4510c6f7acc355241f033f762d7
lpuentes19 <luey@Luiss-MacBook-Pro.local>
2017-04-18 10:19:51 -0600

    SetupPokedex viewcontroller and subclassed

---
+++ Pokedex/Pokedex/PokedexViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PokedexViewController.swift
+//  Pokedex
+//
+//  Created by Luis Puentes on 4/18/17.
+//  Copyright © 2017 LuisPuentes. All rights reserved.
+//
+
+import UIKit
+
+class PokedexViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
